00b4ebceef456dd3d2fbd1bbc5455d1b1d604b94
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-11-25 16:59:36 +0900

    feat: split launch config by use case
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

---
+++ driving_log_replayer_v2/driving_log_replayer_v2/launch/localization.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2024 TIER IV.inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+RECORD_TOPIC = """^/tf$\
+|^/diagnostics$\
+|^/localization/pose_estimator/exe_time_ms$\
+|^/localization/pose_estimator/iteration_num$\
+|^/localization/pose_estimator/transform_probability$\
+|^/localization/pose_estimator/nearest_voxel_transformation_likelihood$\
+|^/localization/pose_estimator/initial_to_result_relative_pose$\
+|^/localization/pose_estimator/ndt_marker$\
+|^/localization/pose_estimator/pose$\
+|^/localization/pose_estimator/pose_with_covariance$\
+|^/localization/kinematic_state$\
+|^/localization/reference_kinematic_state$\
+|^/localization/util/downsample/pointcloud$\
+|^/localization/pose_estimator/points_aligned$\
+|^/driving_log_replayer/.*\
+"""
+
+AUTOWARE_DISABLE = {
+    "perception": "false",
+    "planning": "false",
+    "control": "false",
+}
+
+AUTOWARE_ARGS = {
+    "pose_source": "ndt",
+    "twist_source": "gyro_odom",
+}
+
+NODE_PARAMS = {}
---
+++ driving_log_replayer_v2/driving_log_replayer_v2/launch/obstacle_segmentation.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2024 TIER IV.inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch.substitutions import LaunchConfiguration
+
+RECORD_TOPIC = """^/tf$\
+|^/perception/obstacle_segmentation/pointcloud$\
+|^/planning/scenario_planning/trajectory$\
+|^/planning/scenario_planning/status/stop_reasons$\
+|^/driving_log_replayer_v2/.*\
+"""
+
+AUTOWARE_DISABLE = {
+    "localization": "false",
+    "control": "false",
+}
+
+AUTOWARE_ARGS = {
+    "scenario_simulation": "true",
+}
+
+NODE_PARAMS = {
+    "vehicle_model": LaunchConfiguration("vehicle_model"),
+    "map_path": LaunchConfiguration("map_path"),
+}
