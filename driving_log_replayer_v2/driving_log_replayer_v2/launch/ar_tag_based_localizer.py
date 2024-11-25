503e23822a7f4b0ca1fc14c6ca7dcbfe0c63e809
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-11-25 18:04:54 +0900

    feat: split launch config by use case (#53)
    * feat: split launch config by use case
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    * feat: import launch_config
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    * fix: replace " with '
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    * fix: typo
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    * fix: copyright
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

---
+++ driving_log_replayer_v2/driving_log_replayer_v2/launch/ar_tag_based_localizer.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2024 TIER IV.inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+RECORD_TOPIC = """^/tf$\
+|^/diagnostics$"\
+|^/localization/kinematic_state$\
+"""
+
+AUTOWARE_DISABLE = {
+    "perception": "false",
+    "planning": "false",
+    "control": "false",
+}
+
+AUTOWARE_ARGS = {
+    "pose_source": "artag",
+    "twist_source": "gyro_odom",
+}
+
+NODE_PARAMS = {}
