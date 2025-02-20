b00f53733568d1a570fbb95b3897163af0835b84
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2025-02-20 16:11:39 +0900

    feat: all component test (#90)
    * feat: add all component setting
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    * feat: all component evaluation uses record only mode
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    * add record topic for all component test
    
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>
    
    ---------
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>
    Co-authored-by: MasatoSaeki <masato.saeki@tier4.jp>

---
+++ driving_log_replayer_v2/driving_log_replayer_v2/launch/all.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2025 TIER IV.inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch.actions import DeclareLaunchArgument
+
+RECORD_TOPIC = """^/diagnostics$\
+|^/sensing/lidar/concatenated/pointcloud$\
+|^/perception/object_recognition/objects$\
+|^/perception/obstacle_segmentation/pointcloud$\
+|^/perception/object_recognition/detection/rois(11|10|[0-9])$\
+|^/perception/traffic_light_recognition/camera(11|10|[0-9])/detection/rois$\
+|^/perception/traffic_light_recognition/camera(11|10|[0-9])/detection/rough/rois$\
+|^/perception/traffic_light_recognition/camera(11|10|[0-9])/classification/traffic_signals$\
+|^/perception/traffic_light_recognition/traffic_signals$\
+|^/tf$\
+|^/tf_static$\
+|^/planning/scenario_planning/lane_driving/behavior_planning/path$\
+|^/planning/scenario_planning/trajectory$\
+|^/.*/virtual_wall/.*$\
+|^/.*/path_candidate/.*\
+"""
+
+AUTOWARE_DISABLE = {}
+
+AUTOWARE_ARGS = {
+    "pose_source": "ndt",
+    "twist_source": "gyro_odom",
+}
+
+NODE_PARAMS = {}
+
+USE_CASE_ARGS: list[DeclareLaunchArgument] = []
