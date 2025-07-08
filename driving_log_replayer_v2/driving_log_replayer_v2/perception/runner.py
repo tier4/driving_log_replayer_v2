385ea2ae47904019f154bf7c80bd8dc3102e6322
TadaKazuto <kazuto.tada@tier4.jp>
2025-07-08 17:16:39 +0900

    Remove comment

--- driving_log_replayer_v2/driving_log_replayer_v2/perception/runner.py
+++ driving_log_replayer_v2/driving_log_replayer_v2/perception/runner.py
@@ -387,7 +387,6 @@ def evaluate(
                 continue
 
         # Process perception messages (DetectedObjects, TrackedObjects, PredictedObjects)
-        """
         process_perception_message(
             msg,
             topic_name,
@@ -400,7 +399,7 @@ def evaluate(
             additional_record_topic_name,
             degradation_topic,
         )
-        """
+        
     rosbag_manager.close_writer()
 
     logging.info(f"evaluation topics end")
