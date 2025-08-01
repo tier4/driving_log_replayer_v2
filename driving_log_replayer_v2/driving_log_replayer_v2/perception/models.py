806edaddd8b09fa7497e029ddd02ce497237d23c
MasatoSaeki <masato.saeki@tier4.jp>
2025-08-02 04:34:22 +0900

    fix
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>

--- driving_log_replayer_v2/driving_log_replayer_v2/perception/models.py
+++ driving_log_replayer_v2/driving_log_replayer_v2/perception/models.py
@@ -166,7 +166,7 @@ class Conditions(BaseModel):
 
 class Evaluation(BaseModel):
     UseCaseName: Literal["perception"]
-    UseCaseFormatVersion: Literal["1.0.0", "1.1.0"]
+    UseCaseFormatVersion: Literal["1.0.0", "1.1.0", "1.2.0"]
     Datasets: list[dict]
     Conditions: Conditions
     PerceptionEvaluationConfig: dict
