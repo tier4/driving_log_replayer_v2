a7541b6ec06d5929c18a46141de827adad42c42d
badai-nguyen <dai.nguyen@tier4.jp>
2025-09-19 22:08:40 +0900

    refactor
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>

--- driving_log_replayer_v2/driving_log_replayer_v2/launch/camera_2d_detector.py
+++ driving_log_replayer_v2/driving_log_replayer_v2/launch/camera_2d_detector.py
@@ -1,28 +1,44 @@
-from importlib import import_module
+# Copyright (c) 2025 TIER IV.inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from pathlib import Path
-
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchContext
-from launch.actions import LogInfo
-
-from launch_ros.actions import PushRosNamespace
-from launch.actions import GroupAction
-from launch_ros.actions import ComposableNodeContainer
+from launch.actions import LogInfo, GroupAction
+from launch_ros.actions import PushRosNamespace, ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 from launch import LaunchContext
-from launch.actions import LogInfo
 
 
 
-def create_2d_detector_container(context, camera_id=0, image_type="image_raw"):
+def create_2d_detector_container(context, camera_id: str, image_type: str = "image_raw"):
+    """
+    Create a container with composable nodes for 2D object detection from camera images.
+    Args:
+        context: LaunchContext containing launch configurations.
+        camera_id: Camera identifier (e.g., '0', '1').
+        image_type: Type of image topic (e.g., 'image_raw', 'image_rect_color').
+    Returns:
+        GroupAction containing the detector container.
+    """
     conf = context.launch_configurations
     use_bytetrack = conf["use_bytetrack"]
-    ns = "/perception/object_recognition/detection"
-    yolox_ns = ns
-    if use_bytetrack == "true":
-        yolox_ns = "/perception/object_recognition/detection/yolox"
+    base_ns = "/perception/object_recognition/detection"
+    yolox_ns = base_ns if use_bytetrack != "true" else f"{base_ns}/yolox"
     input_image_topic = f"/sensing/camera/camera{camera_id}/{image_type}"
-    yolox_output_topics = yolox_ns + "/rois"
+    yolox_output_topic = f"{yolox_ns}/rois"
+
+    # YOLOX node
     yolox_package_dir = Path(get_package_share_directory("autoware_tensorrt_yolox"))
     yolox_param_path = Path(yolox_package_dir, "config", "yolox_s_plus_opt.param.yaml").as_posix()
     yolox_params = {
@@ -32,71 +48,79 @@ def create_2d_detector_container(context, camera_id=0, image_type="image_raw"):
     }
     yolox_node = ComposableNode(
         package="autoware_tensorrt_yolox",
-        plugin="autoware::tensorrt_yolox::TrtYoloXNode",  # Adjust plugin name as needed
-        name="tensorrt_yolox_node" + str(camera_id),
-        namespace=ns,
-        parameters=[
-            yolox_param_path, yolox_params
-        ],
+        plugin="autoware::tensorrt_yolox::TrtYoloXNode",
+        name=f"tensorrt_yolox_node{camera_id}",
+        namespace=base_ns,
+        parameters=[yolox_param_path, yolox_params],
         remappings=[
             ("~/in/image", input_image_topic),
-            ("~/out/objects", yolox_output_topics + camera_id),
+            ("~/out/objects", f"{yolox_output_topic}{camera_id}"),
         ],
     )
 
-    decompress_param_path=Path(
+    # Decompressor node
+    decompress_param_path = Path(
         get_package_share_directory("autoware_image_transport_decompressor"),
         "config",
         "image_transport_decompressor.param.yaml",
     ).as_posix()
-    decompresor_node = ComposableNode(
+    decompressor_node = ComposableNode(
         package="autoware_image_transport_decompressor",
         plugin="autoware::image_preprocessor::ImageTransportDecompressor",
-        name="decompress_node" + str(camera_id),
-        namespace=ns,
+        name=f"decompress_node{camera_id}",
+        namespace=base_ns,
         parameters=[decompress_param_path],
         remappings=[
-            ("~/input/compressed_image", input_image_topic + "/compressed"),
+            ("~/input/compressed_image", f"{input_image_topic}/compressed"),
             ("~/output/raw_image", input_image_topic),
         ],
     )
 
-    composable_nodes = [yolox_node, decompresor_node]
+    composable_nodes = [yolox_node, decompressor_node]
 
-    bytetrack_package_dir = Path(get_package_share_directory("autoware_bytetrack"))
-    bytetrack_param_path = Path(bytetrack_package_dir, "config", "bytetrack.param.yaml").as_posix()
+    # Optional ByteTrack node
     if use_bytetrack == "true":
+        bytetrack_package_dir = Path(get_package_share_directory("autoware_bytetrack"))
+        bytetrack_param_path = Path(bytetrack_package_dir, "config", "bytetrack.param.yaml").as_posix()
         bytetrack_node = ComposableNode(
             package="autoware_bytetrack",
             plugin="autoware::bytetrack::ByteTrackNode",
-            name="bytetrack_node" + str(camera_id),
-            namespace=ns,
-            parameters=[
-                bytetrack_param_path,
-            ],
+            name=f"bytetrack_node{camera_id}",
+            namespace=base_ns,
+            parameters=[bytetrack_param_path],
             remappings=[
-                ("~/in/rect", yolox_ns + "/rois" + str(camera_id)),
-                ("~/out/objects", ns + "/rois" + str(camera_id)),
-                ("~/out/objects/debug/uuid", ns + "/rois" + str(camera_id) + "/rois/debug/uuid"),
+                ("~/in/rect", f"{yolox_ns}/rois{camera_id}"),
+                ("~/out/objects", f"{base_ns}/rois{camera_id}"),
+                ("~/out/objects/debug/uuid", f"{base_ns}/rois{camera_id}/rois/debug/uuid"),
             ],
         )
         composable_nodes.append(bytetrack_node)
 
-    container =  ComposableNodeContainer(
-        name="camera_2d_detector_container" + str(camera_id),
+    container = ComposableNodeContainer(
+        name=f"camera_2d_detector_container{camera_id}",
         namespace="",
         package="rclcpp_components",
         executable="component_container",
         composable_node_descriptions=composable_nodes,
         output="screen",
     )
-    return GroupAction([PushRosNamespace(ns), container])
+    return GroupAction([PushRosNamespace(base_ns), container])
 
 
 def launch_camera_2d_detector(context: LaunchContext) -> list:  # for launching tensorrt_yolox
+    """
+    Launch 2D detector containers for all specified cameras if enabled in configuration.
+    Args:
+        context: LaunchContext containing launch configurations.
+    Returns:
+        List of GroupAction objects for each camera detector container.
+    """
     conf = context.launch_configurations
-    if conf["with_2d_detector"] != "true":
+    if conf.get("with_2d_detector") != "true":
         return [LogInfo(msg="2D detector is not launched.")]
     camera_ids = conf["camera_ids"].split(",")
-    camera_2d_detector_containers = [create_2d_detector_container(context, camera_id=camera_id, image_type="image_raw") for camera_id in camera_ids]
-    return camera_2d_detector_containers
+    image_type = conf.get("image_type", "image_raw")
+    return [
+        create_2d_detector_container(context, camera_id=camera_id, image_type=image_type)
+        for camera_id in camera_ids
+    ]
