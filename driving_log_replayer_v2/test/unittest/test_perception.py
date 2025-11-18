8e492681c15f7f3c5391f15feac0f38efa5326df
MasatoSaeki <masato.saeki@tier4.jp>
2025-11-18 18:03:56 +0900

    pre-commit
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>

--- driving_log_replayer_v2/test/unittest/test_perception.py
+++ driving_log_replayer_v2/test/unittest/test_perception.py
@@ -42,8 +42,13 @@ def test_scenario() -> None:
     scenario: PerceptionScenario = load_sample_scenario("perception", PerceptionScenario)
     assert scenario.Evaluation.Conditions.Criterion[0].CriteriaMethod == "num_gt_tp"
     assert scenario.Evaluation.Conditions.Criterion[1].CriteriaLevel == "easy"
-    assert scenario.include_use_case.Conditions.PlanningFactorConditions[0].topic == "/planning/planning_factors/obstacle_stop"
-    assert scenario.include_use_case.Conditions.PlanningFactorConditions[0].time.start == 1649138860.0
+    assert (
+        scenario.include_use_case.Conditions.PlanningFactorConditions[0].topic
+        == "/planning/planning_factors/obstacle_stop"
+    )
+    assert (
+        scenario.include_use_case.Conditions.PlanningFactorConditions[0].time.start == 1649138860.0
+    )
 
 
 def test_scenario_criteria_custom_level() -> None:
