e03fce4cfe040aa8b4cb0ff92713d163256519d9
Kyoichi Sugahara <kyoichi.sugahara@tier4.jp>
2026-03-13 11:41:49 +0900

    fix: filter topics in RosbagReader initialization based on provided topic list
    Signed-off-by: Kyoichi Sugahara <kyoichi.sugahara@tier4.jp>

--- driving_log_replayer_v2/driving_log_replayer_v2/rosbag.py
+++ driving_log_replayer_v2/driving_log_replayer_v2/rosbag.py
@@ -37,8 +37,11 @@ class RosbagReader:
         self._reader = SequentialReader()
         self._reader.open(storage_options, converter_options)
         self._topic_list = topic_list
+        topic_name_set = set(topic_list)
         self._topic_name2type: dict[str, Any] = {
-            topic.name: get_message(topic.type) for topic in self._reader.get_all_topics_and_types()
+            topic.name: get_message(topic.type)
+            for topic in self._reader.get_all_topics_and_types()
+            if topic.name in topic_name_set
         }
 
     def get_topic_name2type(self) -> dict[str, Any]:
