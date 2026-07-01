ef056baba62c3f85e695822f011dcfbd7f58a685
MasatoSaeki <masato.saeki@tier4.jp>
2026-07-01 19:37:49 +0900

    update perception format version
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>

--- driving_log_replayer_v2/driving_log_replayer_v2/perception/models.py
+++ driving_log_replayer_v2/driving_log_replayer_v2/perception/models.py
@@ -146,7 +146,7 @@ class Conditions(BaseModel):
 
 class Evaluation(BaseModel):
     UseCaseName: Literal["perception"]
-    UseCaseFormatVersion: Literal["1.0.0", "1.1.0", "1.2.0", "1.3.0", "1.4.0", "1.5.0"]
+    UseCaseFormatVersion: Literal["1.0.0", "1.1.0", "1.2.0", "1.3.0", "1.4.0", "1.5.0", "1.6.0"]
     Datasets: list[dict]
     Conditions: Conditions
     PerceptionEvaluationConfig: dict
